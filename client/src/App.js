f1b1c214c9cb463dbb03ea1cbc8c3f7eeb6bf3ed
Paul Montgomery <c_pmontgomery@uwm.com>
2021-10-26 13:35:59 -0700

    CORS works!

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,42 @@
-import './App.css';
-const axios = require('axios');
+import "./App.css";
+const axios = require("axios");
 
 function App() {
-
-
   function doFetch() {
-
-
-
-    var data = JSON.stringify({ "query": "query Query {postsByUser(userId:\"kermitt\") {content} currentUser { posts{ content, userId}}}" });
+    var data = JSON.stringify({
+      query:
+        'query Query {postsByUser(userId: "mulefish") {content}    currentUser {posts {content, userId}}}',
+    });
 
     var config = {
-      method: 'post',
-      url: 'localhost:4001/',
-      //url: 'https://reqbin.com/echo/post/json',
+      method: "post",
+      url: "http://localhost:4001",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      data: data
+      data: data,
     };
 
     axios(config)
       .then(function (response) {
         console.log(JSON.stringify(response.data));
+        alert("Look at the console! ");
       })
       .catch(function (error) {
+        alert("oh no " + error);
         console.log(error);
       });
-
-
-
-
   }
-
 
   return (
     <div className="App">
-
-      <button onClick={() => { doFetch() }}>Fetch</button>
-
+      <button
+        onClick={() => {
+          doFetch();
+        }}
+      >
+        Fetch!!
+      </button>
 
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
--- server/data.js
+++ server/data.js
@@ -1,36 +1,36 @@
 const posts = [
-    {
-        id: 1,
-        content: "This is some content here...",
-        userId: "mulefish"
-    },
-    {
-        id: 2,
-        content: "Zoom...",
-        userId: "mulefish"
-    },
-    {
-        id: 3,
-        content: "Red Brown Dirt",
-        userId: "mulefish"
-    },
-    {
-        id: 4,
-        content: "I'm a cat",
-        userId: "kermitt"
-    }
-]
+  {
+    id: 1,
+    content: "This is some content here...",
+    userId: "mulefish",
+  },
+  {
+    id: 2,
+    content: "GOAT...",
+    userId: "mulefish",
+  },
+  {
+    id: 3,
+    content: "Red Brown Dirt",
+    userId: "mulefish",
+  },
+  {
+    id: 4,
+    content: "I'm a cat",
+    userId: "kermitt",
+  },
+];
 
 const users = [
-    {
-        id: "mulefish",
-        username: "mulefish"
-    },
-    {
-        id: "kermitt",
-        username: "kermitt"
-    }
-]
+  {
+    id: "mulefish",
+    username: "mulefish",
+  },
+  {
+    id: "kermitt",
+    username: "kermitt",
+  },
+];
 
 exports.users = users;
 exports.posts = posts;
