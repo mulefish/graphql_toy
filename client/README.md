5b5e33a4e8cd11751433d7124eb16d2794c53826
kermitt PDX <bluefishbrowndirt@gmail.com>
2021-10-26 14:47:17 -0500

    working it...

--- client/README.md
+++ client/README.md
@@ -1,3 +1,6 @@
 
+
+# NOTE TO SELF: 
+Use nvm to bump the node version down. 
 source .bashrc 
- nvm install 16.12.0   
+nvm install 16.12.0   
--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+const axios = require('axios');
 
 function App() {
+
+
+  function doFetch() {
+
+
+
+    var data = JSON.stringify({ "query": "query Query {postsByUser(userId:\"kermitt\") {content} currentUser { posts{ content, userId}}}" });
+
+    var config = {
+      method: 'post',
+      url: 'localhost:4001/',
+      //url: 'https://reqbin.com/echo/post/json',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: data
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+
+
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
+
+      <button onClick={() => { doFetch() }}>Fetch</button>
+
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +53,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
